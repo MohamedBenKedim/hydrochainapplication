d4bd3550adf48a0fb13650f6237c7a1ae225e36e
mohamedbenkedim <mohamedbenkedim@gmail.com>
2024-08-13 21:39:21 +0100

    Revert "solved style"
    This reverts commit 2000fe08e9853265e80fa083a2dfa65a82f0c4c2.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-import ClientOnly from '../Components/ClientOnly'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,16 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClientOnly>
-          <ThemeProvider
+        <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
             {children}
-          </ThemeProvider>
-        </ClientOnly>
+        </ThemeProvider>
       </body>
     </html>
   );
